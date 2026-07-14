9e00c47640f3137d5edc7519445e04c6a5a52a40
Ard Rotterdam <ardrotterdam@gmail.com>
2026-07-14 12:00:45 +0200

    add google analytics 4 tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { Cormorant_Garamond, Inter } from "next/font/google";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
@@ -48,6 +49,7 @@ export default function RootLayout({
         <JsonLd data={jsonLd} />
         <Header />
         {children}
+        <GoogleAnalytics gaId="G-FY6TDYHB0P" />
         <Footer />
       </body>
     </html>
